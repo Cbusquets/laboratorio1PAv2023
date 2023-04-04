6b1a2cbf9b80c9dc144617fbacefd3b13072f492
Maité <martinezf.maite@gmail.com>
2023-04-03 23:05:43 -0300

    Agrega el main y la division de los strings para llamar a las funciones

--- Definiciones.h
+++ Definiciones.h
@@ -1,6 +1,7 @@
-#ifndef DEFINICIONES 
-#define DEFINICIONES 
+#ifndef DEFINICIONES
+#define DEFINICIONES
 
+#define MAX_COMANDO 130
 #define MAX_SOCIOS 50
 #define MAX_CLASES 50
 
--- main.cpp
+++ main.cpp
@@ -1,1 +1,104 @@
-//Hola
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include "Definiciones.h"
+
+using namespace std;
+
+int main()
+{
+
+    char *comando = new (char[MAX_COMANDO]);
+    char *opc, *param1 = NULL, *param2 = NULL, *param3 = NULL;
+    bool salir = false;
+
+    do
+    {
+        param1 = NULL;
+        param2 = NULL;
+        param3 = NULL;
+        cout << "\nElegi la opcion que desees\n \n";
+        cout << "agregarSocio(ci, nombre)\n";
+        cout << "agregarClase(clase)\n";
+        cout << "agregarInscripcion(ciSocio, idClase, fecha)\n";
+        cout << "borrarInscripcion(ciSocio, idClase)\n";
+        cout << "salir \n\n";
+
+        fflush(stdin);
+        fgets(comando, MAX_COMANDO, stdin);
+
+        opc = strtok(comando, "( ,)");
+
+        if (strcasecmp(opc, "agregarSocio") == 0)
+        {
+            param1 = strtok(NULL, "( ,)");
+            param2 = strtok(NULL, "( ,)");
+
+            if ((param1 != NULL) && (param2 != NULL))
+            {
+                // To-DO llamada a agregarSocio()
+                cout << "Funcion " << opc << " con los parametros " << param1 << " " << param2 << "\n";
+            }
+            else
+            {
+                cout << "ERROR - Faltan parámetros\n";
+            }
+        }
+        else if (strcasecmp(opc, "agregarClase") == 0)
+        {
+            param1 = strtok(NULL, "( ,)");
+
+            if (param1 != NULL)
+            {
+                // To-DO llamada a agregarClase()
+                cout << "Funcion " << opc << " con los parametros " << param1 << "\n";
+            }
+            else
+            {
+                cout << "ERROR - Faltan parámetros\n";
+            }
+        }
+        else if (strcasecmp(opc, "agregarInscripcion") == 0)
+        {
+            param1 = strtok(NULL, "( ,)");
+            param2 = strtok(NULL, "( ,)");
+            param3 = strtok(NULL, "( ,)");
+
+            if ((param1 != NULL) && (param2 != NULL) && (param3 != NULL))
+            {
+                // To-DO llamada a agregarInscripcion()
+                cout << "Funcion " << opc << " con los parametros " << param1 << " " << param2 << " " << param3 << "\n";
+            }
+            else
+            {
+                cout << "ERROR - Faltan parámetros\n";
+            }
+        }
+        else if (strcasecmp(opc, "borrarInscripcion") == 0)
+        {
+            param1 = strtok(NULL, "( ,)");
+            param2 = strtok(NULL, "( ,)");
+
+            if ((param1 != NULL) && (param2 != NULL))
+            {
+                // To-DO llamada a borrarInscripcion()
+                cout << "Funcion " << opc << " con los parametros " << param1 << " " << param2 << "\n";
+            }
+            else
+            {
+                cout << "ERROR - Faltan parámetros\n";
+            }
+        }
+        else if (strcasecmp(opc, "salir") == 0)
+        {
+            salir = true;
+        }
+        else
+        {
+            cout << "ERROR - Opcion incorrecta";
+        }
+
+    } while (!salir);
+
+    delete[] comando;
+}
